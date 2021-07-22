a8fc1f0b43a6cca42f156c0ac7f0e34c84864060
feifei <tianbian5@163.com>
2021-07-22 23:19:19 +0800

    [bundle]统一导出文件入口，注意符合组件Menu的导出入口文件写法

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,4 +1,16 @@
-import Menu from "./menu";
-import MenuItem from "./menuItem";
+import { FC } from "react";
+import Menu, { IMenuProps } from "./menu";
+import SubMenu, { ISubMenuProps } from "./subMenu";
+import MenuItem, { IMenuItemProps } from "./menuItem";
 
-export { Menu, MenuItem };
+export type IMenuComponent = FC<IMenuProps> & {
+  // yyfkey:// 符合组件的写法
+  Item: FC<IMenuItemProps>;
+  SubMenu: FC<ISubMenuProps>;
+};
+
+const TransMenu = Menu as IMenuComponent;
+TransMenu.Item = MenuItem;
+TransMenu.SubMenu = SubMenu;
+
+export default TransMenu;
